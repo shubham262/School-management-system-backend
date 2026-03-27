cc9b468640149e57d6edf45f38472878cdc5d4e2
Shubham PC <shubhampc@Shubhams-MacBook-Air.local>
2026-03-28 00:55:41 +0530

    better auth setup ready

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,7 +1,7 @@
 import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { handleMongoDbConnection } from "./index.js";
-
+import { jwt, bearer } from "better-auth/plugins";
 export const handleBetterAuth = async () => {
 	const { db } = await handleMongoDbConnection();
 	const auth = betterAuth({
@@ -9,7 +9,19 @@ export const handleBetterAuth = async () => {
 		emailAndPassword: {
 			enabled: true,
 		},
-		baseURL: "http://localhost:5000",
+		user: {
+			additionalFields: {
+				role: {
+					type: ["user", "admin"],
+					required: false,
+					defaultValue: "user",
+					input: false, // don't allow user to set role
+				},
+			},
+		},
+		plugins: [jwt(), bearer()],
+
+		baseURL: "http://localhost:3001",
 	});
 
 	return auth;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { handleBetterAuth } from "./config/auth.js";
 import { toNodeHandler } from "better-auth/node";
 const app = express();
 const auth = await handleBetterAuth();
-app.all("/api/auth/", toNodeHandler(auth));
-app.listen(5000, () => {
-	console.log("Server started at port 5000");
+app.use("/api/auth", toNodeHandler(auth));
+app.listen(3001, () => {
+	console.log("Server started at port 3001");
 });
